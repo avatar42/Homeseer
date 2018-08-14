be1c4d87f1fcbe878180567923d98beca9b72ecc
avatar42 avatar42_github@m42.biz <_github@m42.biz>
2018-08-14 15:58:17 -0500

    Add script for  sending  alerts to PushBullet

---
+++ scripts/PushBullet.vb
@@ -0,0 +1,61 @@
+﻿'load object refs and speech methods
+#Include SayIt.vb
+
+' import headers def
+#Include Secrets.vb
+
+Sub Main(ByVal params As String) 
+    SendMsg(params)
+End Sub
+
+'Dim AlarmOff = 0
+'Dim AlarmHome = 1
+'Dim AlarmPerm = 2
+'Dim AlarmAway = 3
+'Dim AlarmLevel2899Ref = 2899
+Sub SendMsgIfAway(ByVal params As String) 
+    Dim label = "SendMsgIfAway"
+    hs.WriteLog(label, "AlarmLevel2899Ref:" & hs.DeviceValue(AlarmLevel2899Ref))
+
+    if hs.DeviceValue(AlarmLevel2899Ref) >= AlarmAway Then
+        SendMsg(params & " in away mode")
+    Else if hs.DeviceValue(AlarmLevel2899Ref) >= AlarmHome Then
+        Dim paramArr()
+        paramArr = params.Split("#")
+        sayString(paramArr(0) & " " & paramArr(1))
+    End if
+End Sub
+
+' Send if alarm in perimeter mode
+Sub SendMsgIfPerm(ByVal params As String) 
+    Dim label = "SendMsgIfPerm"
+    hs.WriteLog(label, "AlarmLevel2899Ref:" & hs.DeviceValue(AlarmLevel2899Ref))
+
+    if hs.DeviceValue(AlarmLevel2899Ref) >= AlarmAway Then
+        SendMsgIfAway(params)
+    Else if hs.DeviceValue(AlarmLevel2899Ref) >= AlarmPerm Then
+        SendMsg(params & " in perimeter mode")
+    Else if hs.DeviceValue(AlarmLevel2899Ref) >= AlarmHome Then
+        Dim paramArr()
+        paramArr = params.Split("#")
+        sayString(paramArr(0) & " " & paramArr(1))
+    End if
+End Sub
+
+Sub SendMsg(ByVal params As String) 
+    Dim label = "SendMsg"
+    dim s, data, title, message 
+    Dim paramArr() 
+    const server_url = "https://api.pushbullet.com/v2/pushes" 
+
+    hs.WriteLog(label,"params:" & params)
+    paramArr = params.Split("#") 
+    title = paramArr(0) 
+    message = paramArr(1) 
+
+    data = "type=note&title="+title+"&body="+message 
+
+    s = hs.URLAction(server_url, "POST", data, headers) 
+
+    sayAlert(title & " " & message)
+End Sub
---
+++ scripts/Secrets.vb
@@ -0,0 +1,8 @@
+' See https://docs.pushbullet.com/v2/ on how to get a token
+''curl -u XXXXXXXXXXXXXXXXXXXXXX: https://api.pushbullet.com/v2/pushes -d type=note -d title="Alert" -d body="push test"
+const headers="Access-Token: XXXXXXXXXXXXXXXXXXXXXX" 
+
+
+Public Sub loaded(ByVal delta As Object)
+    hs.WriteLog("Secrets","Secrets loaded")
+End Sub
