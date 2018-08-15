cd0e0fc09e4d1aa458a3f4cad6b1340b65bc5ecd
avatar42 avatar42_github@m42.biz <_github@m42.biz>
2018-08-14 20:04:38 -0500

    Add comments

--- scripts/PushBullet.vb
+++ scripts/PushBullet.vb
@@ -4,15 +4,22 @@
 ' import headers def
 #Include Secrets.vb
 
+' Methods for sending messages to Pushbullet
+
 Sub Main(ByVal params As String) 
     SendMsg(params)
 End Sub
 
+'Refs used
 'Dim AlarmOff = 0
 'Dim AlarmHome = 1
 'Dim AlarmPerm = 2
 'Dim AlarmAway = 3
 'Dim AlarmLevel2899Ref = 2899
+
+'params in format of title#message you want to send
+
+' Send if alarm in away mode
 Sub SendMsgIfAway(ByVal params As String) 
     Dim label = "SendMsgIfAway"
     hs.WriteLog(label, "AlarmLevel2899Ref:" & hs.DeviceValue(AlarmLevel2899Ref))
@@ -26,7 +33,7 @@ Sub SendMsgIfAway(ByVal params As String)
     End if
 End Sub
 
-' Send if alarm in perimeter mode
+' Send if alarm in perimeter or away mode
 Sub SendMsgIfPerm(ByVal params As String) 
     Dim label = "SendMsgIfPerm"
     hs.WriteLog(label, "AlarmLevel2899Ref:" & hs.DeviceValue(AlarmLevel2899Ref))
@@ -42,6 +49,7 @@ Sub SendMsgIfPerm(ByVal params As String)
     End if
 End Sub
 
+' Send message no matter mode alarm is in
 Sub SendMsg(ByVal params As String) 
     Dim label = "SendMsg"
     dim s, data, title, message 
