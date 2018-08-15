2e67675a359e6904a73e3114f52481853fa2bd64
avatar42 avatar42_github@m42.biz <_github@m42.biz>
2018-08-14 20:00:32 -0500

    Add speak temp script

---
+++ scripts/SpeakTemp.vb
@@ -0,0 +1,12 @@
+﻿'load object refs and speech methods
+#Include SayIt.vb
+
+'Say the name, value (as temp) and last change time of a device.
+Public Sub SpeakTemp(ByVal devRef As Object)  
+    if (hs.DeviceExistsRef(devRef) ) Then
+        sayString("Sensor " & hs.DeviceName(devRef) & " has a temperature of " & hs.DeviceValue(devRef) & " degrees as of " & hs.DeviceLastChangeRef(devRef))
+    Else
+        sayString("Sensor " & devRef & " not found " )
+    End If
+End Sub  
+ 
