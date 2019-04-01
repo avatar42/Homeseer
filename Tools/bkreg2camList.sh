9bbac0dc8e00d68a0e7fedbb1d4af517ff44f8c1
avatar42 avatar42_github@m42.biz <_github@m42.biz>
2019-04-01 16:47:15 -0500

    Make username and password match blog bpost

--- Tools/bkreg2camList.sh
+++ Tools/bkreg2camList.sh
@@ -3,12 +3,13 @@ echo  host,shortname,dsname,fullxres,fullyres,HS3ref,vcodec,folder,enabled,group
 set -x
 for host in BlueIris2 BlueIris3 BlueIris4 BlueIris5
 do
-	iconv -f UTF-16LE -t UTF-8  ${host}.reg | grep -E "\"enabled\"|\"group\"|shortname|dsname|fullxres|fullyres|&value=3|\"vcodec\"|\"folder\"" | sed -e "s|10.10.1.45/JSON?user=rpi&pass=this1ok&request=controldevicebyvalue&ref=||g" | sed -e "s|&value=3||g" > ${host}.raw
+	iconv -f UTF-16LE -t UTF-8  ${host}.reg | grep -E "\"enabled\"|\"group\"|shortname|dsname|fullxres|fullyres|&value=3|\"vcodec\"|\"folder\"" | sed -e "s|10.10.1.45/JSON?user=userName&pass=P@ssw0rd&request=controldevicebyvalue&ref=||g" | sed -e "s|&value=3||g" > ${host}.raw
 	echo ======== Raw =========
 	cat ${host}.raw
 	echo ======== Raw =========
 
-	cut -f2-20 -d'=' ${host}.raw | sed -e "s|||g" | sed -e "s|cameraId=1538607685:1\"|cameraId=1538607685:1\"\ndword:00000280\ndword:000001e0|g" | sed -e "s|dword:||g" > ${host}.tmp
+	cut -f2-20 -d'=' ${host}.raw | sed -e "s|
+||g" | sed -e "s|cameraId=1538607685:1\"|cameraId=1538607685:1\"\ndword:00000280\ndword:000001e0|g" | sed -e "s|dword:||g" > ${host}.tmp
 	echo ======== Tmp =========
 	cat ${host}.tmp
 	echo ======== Tmp =========
