dd539cb2dad5a0a5bb44c56e7999df53a543b748
avatar42 avatar42_github@m42.biz <_github@m42.biz>
2019-04-06 20:03:51 -0500

    Add Sub to export most of the info about devices to a csv file

--- scripts/ListDevices.vb
+++ scripts/ListDevices.vb
@@ -1,4 +1,5 @@
 ﻿#Include Refs.vb
+Const Quote = """"
 ' look at all the devices and set virtutals to number of devices, highest ref number and sum of ref numbers so an event can do a check and announce 
 ' if something changes.
 ' Mainly this is to monitor some plugins that seem to recreate devices after updates.
@@ -35,9 +36,83 @@ Public Sub DevChk(Parms As Object)
         Loop Until EN.Finished
         hs.WriteLog(label, "cnt:  " & cnt & " max: " & max & " total: " & total)
         hs.SetDeviceValueByRef(DeviceCount1522Ref, cnt, True)
-        hs.SetDeviceValueByRef(LastDevice2975Ref, max , True)
+        hs.SetDeviceValueByRef(LastDevice2975Ref, max, True)
         hs.SetDeviceValueByRef(DeviceChksum4597Ref, total, True)
     Catch ex As Exception
         hs.WriteLog("Error", "Exception in script " & label & ":  " & ex.Message)
     End Try
+End Sub
+
+Public Function SafeString(ByVal value As Object) As String
+    Return Quote & CStr(value).Replace(Quote, "'") & Quote & ","
+End Function
+
+' Create a sheet with info about devices including what does not show on the devices management screen
+Public Sub exportDevAttrs(Parms As Object)
+    Dim label As String = "exportDevAttrs"
+    Dim file As System.IO.StreamWriter
+
+    hs.WriteLog(label, "Starting " & label)
+    Try
+
+        Dim EN As Scheduler.Classes.clsDeviceEnumeration = hs.GetDeviceEnumerator  'Get all devices
+        Dim dv As Scheduler.Classes.DeviceClass
+
+        If EN Is Nothing Then
+            hs.WriteLog(label, "Error getting Enumerator")
+            Exit Sub
+        End If
+        file = My.Computer.FileSystem.OpenTextFileWriter("./html/reports/exportDevAttrs." & Now.ToString("yyMMdd") & ".csv", False)
+        'Status page stuff
+        Dim header As String = "Ref,Image,Status,Cat,Room,Name,Address,Type,Last_Change,Value,Attention,VoiceCommand,"
+        'MISC bits
+        header = header & "BIT0,BIT1,BIT2,NO_LOG,STATUS_ONLY,HIDDEN,BIT6,INCLUDE_POWERFAIL,SHOW_VALUES,AUTO_VOICE_COMMAND,VOICE_COMMAND_CONFIRM,"
+        header = header & "MYHS_DEVICE_CHANGE_NOTIFY,SET_DOES_NOT_CHANGE_LAST_CHANGE,BIT13,BIT14,BIT15,BIT16,"
+        header = header & "NO_STATUS_TRIGGER,NO_GRAPHICS_DISPLAY,NO_STATUS_DISPLAY,CONTROL_POPUP,"
+        ' Other values
+        header = header & "Device_API,SubType,SubType_Description,ImageLarge,Note,"
+        file.WriteLine(header)
+
+        Do  'check each device that was enumerated
+            dv = EN.GetNext
+            If dv Is Nothing Then  'No device, so quit
+                hs.WriteLog(label, "No devices found")
+                Exit Sub
+            End If
+
+            'Status page stuff 
+            Dim attrs As String = SafeString(dv.Ref(Nothing)) & SafeString(dv.Image(Nothing)) & SafeString(dv.devString(Nothing)) & SafeString(dv.Location2(Nothing))
+            attrs = attrs & SafeString(dv.Location(Nothing)) & SafeString(dv.Name(Nothing)) & SafeString(dv.Address(Nothing)) & SafeString(dv.Device_Type_String(Nothing))
+            attrs = attrs & SafeString(dv.Last_Change(Nothing)) & SafeString(dv.devValue(Nothing)) & SafeString(dv.Attention(Nothing)) & SafeString(dv.VoiceCommand(Nothing))
+            'MISC bits
+            If dv.MISC_Check(hs, 0) Then
+                attrs = attrs & 1 & ","
+            Else
+                attrs = attrs & 0 & ","
+            End If
+            For i As Integer = 1 To 1048576
+                If dv.MISC_Check(hs, i) Then
+                    attrs = attrs & 1 & ","
+                Else
+                    attrs = attrs & 0 & ","
+                End If
+                i = i * 2
+            Next
+            ' Other values
+            Dim DT As New DeviceTypeInfo
+            DT = dv.DeviceType_Get(hs)
+            If DT IsNot Nothing Then
+                attrs = attrs & SafeString(DT.Device_API) & SafeString(DT.Device_SubType) & SafeString(DT.Device_SubType_Description)
+            Else
+                attrs = attrs & ",,,"
+            End If
+            attrs = attrs & SafeString(dv.ImageLarge(Nothing)) & SafeString(dv.UserNote(Nothing))
+            file.WriteLine( attrs)
+        Loop Until EN.Finished
+    Catch ex As Exception
+        hs.WriteLog("Error", "Exception in script " & label & ":  " & ex.Message)
+    Finally
+        file.Close()
+    End Try
+    hs.WriteLog(label, "Done " & label)
 End Sub
